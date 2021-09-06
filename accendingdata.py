3257c67f389be26cc401be94bb9f31944a76d924
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2021-09-06 00:08:18 -0700

    Update accendingdata.py

--- accendingdata.py
+++ accendingdata.py
@@ -10,7 +10,7 @@ from bokeh.plotting import figure, output_file, show
 
 # prepare some data
 N = 4000
-x = np.random.random(size=N) * 110
+u = np.random.random(size=N) * 110
 y = np.random.random(size=N) * 140
 radii = np.random.random(size=N) * 2.5
 colors = [
@@ -26,7 +26,7 @@ TOOLS="crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
 p = figure(tools=TOOLS, x_range=(0,100), y_range=(0,100))
 
 # add a circle renderer with vectorized colors and sizes
-p.circle(x,y, radius=radii, fill_color=colors, fill_alpha=0.6, line_color=None)
+p.circle(u,y, radius=radii, fill_color=colors, fill_alpha=0.6, line_color=None)
 
 # show the results
 show(p)
